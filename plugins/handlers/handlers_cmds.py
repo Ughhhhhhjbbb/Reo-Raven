7c80b581dd04a4a6beebc97720e1d32ea04d58a3
☾︎R̶E̶O̶☽︎♕︎T̶H̶E̶G̶A̶M̶E̶K̶I̶L̶L̶E̶R̶♕︎™ <92641591+Ughhhhhhjbbb@users.noreply.github.com>
2025-01-20 21:20:39 +0530

    handlers_cmds.py

--- plugins/handlers/handlers_cmds.py
+++ plugins/handlers/handlers_cmds.py
@@ -22,6 +22,7 @@ async def handler_gates(client: Client, callback_query: CallbackQuery):
     gates_specials = (
         text_gates_especials
     )
+    
 
     # improve in the future using database
     count_status_gates = (
@@ -57,6 +58,15 @@ async def handler_auths(client: Client, callback_query: CallbackQuery):
             text_gates_auth_2, reply_markup=buttons_auth_page_2
         )
 
+@bot.callback_query_handler(func=lambda call: call.data == "charged_back")
+def back_to_charged_page(call):
+    bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text=text_gates_charged,
+        reply_markup=buttons_charged_page_1
+    )
+    
 
 @Client.on_callback_query(filters.regex("chargeds") | filters.regex("chargeds_2"))
 async def handler_chargeds(client: Client, callback_query: CallbackQuery):
@@ -80,30 +90,7 @@ async def handler_chargeds(client: Client, callback_query: CallbackQuery):
         await callback_query.edit_message_text(
             text_gates_charged_5, reply_markup=buttons_charged_page_5
         )
-        elif callback_query.data == "chargeds_6":
-        await callback_query.edit_message_text(
-            text_gates_charged_6, reply_markup=buttons_charged_page_6
-        )
-        elif callback_query.data == "chargeds_7":
-        await callback_query.edit_message_text(
-            text_gates_charged_7, reply_markup=buttons_charged_page_7
-        )
-        elif callback_query.data == "chargeds_8":
-        await callback_query.edit_message_text(
-            text_gates_charged_8, reply_markup=buttons_charged_page_8
-        )
-        elif callback_query.data == "chargeds_9":
-        await callback_query.edit_message_text(
-            text_gates_charged_9, reply_markup=buttons_charged_page_9
-        )
-        elif callback_query.data == "chargeds_10":
-        await callback_query.edit_message_text(
-            text_gates_charged_10, reply_markup=buttons_charged_page_10
-        )
-        elif callback_query.data == "chargeds_11":
-        await callback_query.edit_message_text(
-            text_gates_charged_11, reply_markup=buttons_charged_page_11
-        )
+       
 
 
      
@@ -111,6 +98,10 @@ async def handler_chargeds(client: Client, callback_query: CallbackQuery):
     filters.regex("specials")
     | filters.regex("specials_2")
     | filters.regex("specials_3")
+    | filters.regex("specials_4")
+    | filters.regex("specials_5")
+    | filters.regex("specials_6")
+    | filters.regex("specials_7")
 )
 async def handler_specials(client: Client, callback_query: CallbackQuery):
     if callback_query.data == "specials":
@@ -125,6 +116,22 @@ async def handler_specials(client: Client, callback_query: CallbackQuery):
         await callback_query.edit_message_text(
             text_gates_especials_3, reply_markup=buttons_specials_page_3
         )
+        elif callback_query.data == "specials_4":
+        await callback_query.edit_message_text(
+            text_gates_especials_4, reply_markup=buttons_specials_page_4
+        )
+        elif callback_query.data == "specials_5":
+        await callback_query.edit_message_text(
+            text_gates_especials_5, reply_markup=buttons_specials_page_5
+        )
+        elif callback_query.data == "specials_6":
+        await callback_query.edit_message_text(
+            text_gates_especials_6, reply_markup=buttons_specials_page_6
+        )
+        elif callback_query.data == "specials_7":
+        await callback_query.edit_message_text(
+            text_gates_especials_7, reply_markup=buttons_specials_page_7
+        )
 
 
 @Client.on_callback_query(filters.regex("tools"))
@@ -137,4 +144,3 @@ async def handler_tools(client: Client, callback_query: CallbackQuery):
 @Client.on_callback_query(filters.regex("exit"))
 async def handler_exit(client: Client, callback_query: CallbackQuery):
     await callback_query.message.delete()
-    
